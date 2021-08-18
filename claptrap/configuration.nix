a26ba277f1c20e5afdc146850183f6b27618722f
Nikos Epping <nikos.epping@protonmail.com>
2021-08-18 22:58:37 +0200

    Add user steffen

--- claptrap/configuration.nix
+++ claptrap/configuration.nix
@@ -18,18 +18,27 @@
 
   virtualisation.docker.enable = true;
 
+  users.mutableUsers = false;
+
   users.users.nikos = {
     isNormalUser = true;
     extraGroups = [ "wheel" "docker" ];
     openssh.authorizedKeys.keyFiles = [ ./nikos_authorized_keys ];
     shell = pkgs.zsh;
   };
-  
+ 
   users.users.jan = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpK0nqpxKtD/nnNPhKkVQf2ab/0hOnec5BrHTjxiFFc/N9Sxhdl/374bUygCvBdB3i+bJO0Y9rAjtBaYwO54QLJTcQKt1y2Za7zLHPoZiFpXxdMXSqb6D96h5BvpZ+nyHdbZtDjwbAREj9+AHIp8zWwG9bN6pCg7zK0ZvEKNM/IdZCcTYXasjpj2EJ8MEaSMjfRwfG/zflHMeHZyuQXO/KbLziSrEGP5Wq2xJkKe4axMQR66KZR5x95lcAyBUO0Ow55s5uMxmAJTdizs4SVOTNaMSCYsQLISkChx1NUSPFY7XM1zf33Zq1mNlZs9ZiHT03p/YBm9U3NZqOh3L4XU3H jan-backup@raspberrypi" ];
     shell = pkgs.zsh;
     home = "/srv/no-backup/jan" ;
+  };
+
+  users.users.steffen = {
+    isNormalUser = true;
+    hashedPassword = "$6$s4W6kLSpGZ$1nnIjU/kpkxs0ZHd/YVdFvC8MemHqB2pp2tyJouqu1JCNgQ7L.IoSBe18dy2rjLzxSHxsQLfWhw8OYWgYZitZ/";
+    shell = pkgs.zsh;
+    home = "/srv/no-backup/steffen" ;
   };
 
   # Select internationalisation properties.
